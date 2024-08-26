130b3dbbea21f448d75db445469b4f5cf1ed8ffb
gabrielrportugal <gabrielrportugal@outlook.com>
2024-08-26 20:19:41 -0300

    feat: add analytics to my portfolio app

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,10 +1,12 @@
 import { ColorModeScript } from '@chakra-ui/react'
+import { Analytics } from '@vercel/analytics/react'
 import { Html, Head, Main, NextScript } from 'next/document'
 import { chakraTheme } from '@/utils'
 
 export default function Document() {
   return (
     <Html lang="pt-BR">
+      <Analytics />
       <Head />
       <body>
         <ColorModeScript
