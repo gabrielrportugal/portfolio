b7c6531dc54dea18f53263a19f9ecf1e13bf15d9
gabrielrportugal <gabrielrportugal@outlook.com>
2024-08-26 20:27:08 -0300

    feat: change provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,10 +4,12 @@ import { ChakraProvider } from '@chakra-ui/react'
 import { chakraTheme } from '@/utils'
 import RootLayout from '@/components/root-layout'
 import { SidebarDrawerProvider } from '@/contexts'
+import { Analytics } from '@vercel/analytics/react'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider resetCSS theme={chakraTheme}>
+      <Analytics />
       <SidebarDrawerProvider>
         <RootLayout>
           <Component {...pageProps} />
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,12 +1,10 @@
 import { ColorModeScript } from '@chakra-ui/react'
-import { Analytics } from '@vercel/analytics/react'
 import { Html, Head, Main, NextScript } from 'next/document'
 import { chakraTheme } from '@/utils'
 
 export default function Document() {
   return (
     <Html lang="pt-BR">
-      <Analytics />
       <Head />
       <body>
         <ColorModeScript
